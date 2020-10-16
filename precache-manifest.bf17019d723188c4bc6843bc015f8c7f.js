c156735173aa87bd01a17c9517eaf7dea07f0335
Junyi Chung <junyichung@junyis-air.home>
2020-10-16 12:52:29 -0700

    Updates

---
+++ precache-manifest.bf17019d723188c4bc6843bc015f8c7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5fd10f32c38412611f3b461ce6944bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "130136e00a5d157489c4",
+    "url": "/static/css/main.a3a26323.chunk.css"
+  },
+  {
+    "revision": "608118624ae7ee197d4b",
+    "url": "/static/js/2.e208ecfd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e208ecfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "130136e00a5d157489c4",
+    "url": "/static/js/main.c0a13f52.chunk.js"
+  },
+  {
+    "revision": "e968eeb8c247134aad12",
+    "url": "/static/js/runtime-main.bdad0aff.js"
+  }
+]);
